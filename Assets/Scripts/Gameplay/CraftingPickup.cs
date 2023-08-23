3cffad603f1f5922e80fb1b9ab93d1513ac71d1f
WaluigiNumberOne <gmolesworth@yahoo.com>
2023-08-23 10:24:01 -0700

    Added CraftingManager script and CraftingConsole obj

---
+++ Assets/Scripts/Gameplay/CraftingPickup.cs
@@ -0,0 +1,23 @@
+﻿using Unity.FPS.Game;
+using UnityEngine;
+
+namespace Unity.FPS.Gameplay
+{
+    public class CraftingPickup : Pickup
+    {
+        //Duct Tape -> 0
+        //Metal -> 1
+        //Wire -> 2
+        //Spring -> 3
+        //Battery -> 4
+        public int type;
+        public int size;    //size of stack
+
+        protected override void OnPicked(PlayerCharacterController player)
+        {
+            FindObjectOfType<CraftingManager>().add(type, size);
+            PlayPickupFeedback();
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Gameplay/Managers/CraftingManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraftingManager : MonoBehaviour
+{
+    string[] names;
+    int[] inventory;
+    //Duct Tape -> 0
+    //Metal -> 1
+    //Wire -> 2
+    //Spring -> 3
+    //Battery -> 4
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inventory = new int[5];
+        names = new string[]{"Duct Tape", "Scrap Metal", "Copper Wire", "Spring", "Battery"};
+        displayInventory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void add(int type, int size)
+    {
+        inventory[type] += size;
+    }
+
+    public void displayInventory()
+    {
+        for(int i = 0; i <= 4; i++)
+        {
+            Debug.Log(names[i] + ": " + inventory[i]);
+        }
+    }
+}
